d27fe6d28b280edb0c551b6891a4b71f2b60bb8b
blankMCU <vexown@gmail.com>
2024-09-13 00:02:53 +0200

    Update the server to handle both UDP and TCP messages

--- CentralApplication/server.py
+++ CentralApplication/server.py
@@ -1,6 +1,8 @@
 import socket
 
-def start_server():
+##########################################################################################
+
+def start_tcp_server():
     # Define the IP address and port for the server
     server_ip = "192.168.1.194"  # My PC's IP address assigned by the Tenda WiFi router DHCP server (may change)
     server_port = 12345          # Port number on which the server will listen for incoming connections (uint16 range)
@@ -38,6 +40,47 @@ def start_server():
         # Close the connection with the client
         client_socket.close()
 
-# Ensure that the `start_server` function is called only when the script is executed directly
+##########################################################################################
+
+def start_udp_server():
+    # Define the IP address and port for the server
+    server_ip = "192.168.1.194"  # My PC's IP address assigned by the Tenda WiFi router DHCP server (may change)
+    server_port = 12345          # Port number on which the server will listen for incoming connections (uint16 range)
+
+    # Create a new socket using IPv4 addressing and UDP
+    # socket.AF_INET: IPv4 addressing
+    # socket.SOCK_STREAM: Socket type used for connectionless communication (suitable for UDP)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind the socket to the specified IP address and port
+    server_socket.bind((server_ip, server_port))
+
+    print(f"UDP server listening on port {server_port}")
+
+    while True:
+        # Receive data from a UDP socket
+        # The input buffer size is 1024 bytes (1 KB)
+        message, client_address = server_socket.recvfrom(1024)
+        message_str = message.decode()  # Decode the byte string to a regular string
+        print(f"Message from {client_address}")
+        print(f"Received UDP message: {message_str}")
+
+##########################################################################################
+
+def main():
+    protocol = input("Choose protocol (TCP or UDP): ")
+
+    if protocol.lower() == "tcp":
+        print("Starting TCP server...")
+        start_tcp_server()
+    elif protocol.lower() == "udp":
+        print("Starting UDP server...")
+        start_udp_server()
+    else:
+        print("Invalid protocol choice.")
+
+##########################################################################################
+
 if __name__ == "__main__":
-    start_server()
+    main()
+    
