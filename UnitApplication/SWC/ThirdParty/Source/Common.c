82abd4e025b339d91712cc8120393764830f7eea
blankMCU <vexown@gmail.com>
2024-09-28 22:47:54 +0200

    Enter Critical Section in CriticalErrorHandler

--- UnitApplication/SWC/ThirdParty/Source/Common.c
+++ UnitApplication/SWC/ThirdParty/Source/Common.c
@@ -10,6 +10,10 @@
 /* Standard includes. */
 #include <stdio.h>
 
+/* Kernel includes */
+#include "FreeRTOS.h"
+#include "task.h"   
+
 /* Misc includes */
 #include "Common.h"
 
@@ -31,6 +35,13 @@
 void CriticalErrorHandler(uint8_t moduleId, uint8_t errorId)
 {
     printf("CRTICIAL ERROR OCCURED. moduleId: %u, errorId: %u. Going into endless loop...\n", moduleId, errorId);
+
+    /* Enter a critical section. The idea here is to prevent leaving this endless loop by disabling all interrupts globally.
+    As stated in FreeRTOS documentation:
+        Preemptive context switches only occur inside an interrupt, so will not occur when interrupts are disabled. Therefore, the 
+        task that called taskENTER_CRITICAL() is guaranteed to remain in the Running state until the critical section is exited, 
+        unless the task explicitly attempts to block or yield (which it should not do from inside a critical section). */ 
+    taskENTER_CRITICAL();
     
     /* Infinite loop to trap execution */
     while(1)
