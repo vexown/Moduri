31e2a1c6f38894c6cdbc3005f276b206d8e16a7a
blankMCU <vexown@gmail.com>
2025-01-23 23:18:06 +0100

    Adapt Flash and Debug scripts for RP2350 (Pico 2)

--- UnitApplication/Flash.sh
+++ UnitApplication/Flash.sh
@@ -2,11 +2,18 @@
 
 echo "Flashing the board using Picoprobe and OpenOCD"
 
-openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000" -c "program output/moduri.elf verify reset exit" 
+# Set the path to the OpenOCD installation directory - we are using the OpenOCD version from RPi repository https://github.com/raspberrypi/openocd
+OPENOCD_PATH="/home/blankmcu/pico/openocd"
+
+$OPENOCD_PATH/src/openocd \
+    -s "/home/blankmcu/pico/openocd/tcl" \
+    -f "$OPENOCD_PATH/tcl/interface/cmsis-dap.cfg" \
+    -f "$OPENOCD_PATH/tcl/target/rp2350.cfg" \
+    -c "adapter speed 5000" \
+    -c "program output/moduri.elf verify reset exit"
 
 # Prompt the user to press any key to exit
 echo "Press any key to exit the script."
-
-# Handle user input
 read -n 1 -s  # Read a single keypress silently
 echo "Exiting the script."
+
--- UnitApplication/StartOpenOCD.sh
+++ UnitApplication/StartOpenOCD.sh
@@ -5,7 +5,7 @@
 #
 # Description:
 # This script starts OpenOCD (Open On-Chip Debugger) with specific configuration files
-# and settings for interfacing with a target board (in this case, the RP2040 microcontroller).
+# and settings for interfacing with a target board (in this case, the RP2350 microcontroller).
 # OpenOCD is commonly used for debugging embedded systems.
 #
 # Usage:
@@ -13,7 +13,14 @@
 #
 # ****************************************************************************
 
-openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000"
+# Set the path to the OpenOCD installation directory - we are using the OpenOCD version from RPi repository https://github.com/raspberrypi/openocd
+OPENOCD_PATH="/home/blankmcu/pico/openocd"
+
+$OPENOCD_PATH/src/openocd \
+    -s "/home/blankmcu/pico/openocd/tcl" \
+    -f "$OPENOCD_PATH/tcl/interface/cmsis-dap.cfg" \
+    -f "$OPENOCD_PATH/tcl/target/rp2350.cfg" \
+    -c "adapter speed 5000"
 
 # Explanation of each part of the command:
 
@@ -27,19 +34,19 @@ openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "adapter speed 5000"
 # supported by many ARM microcontroller debuggers. It configures OpenOCD to use the CMSIS-DAP adapter
 # to communicate with the target microcontroller.
 
-# -f target/rp2040.cfg
+# -f target/rp2350.cfg
 # This option specifies the configuration file for the target device (the microcontroller being debugged).
-# 'rp2040.cfg' is the configuration file for the Raspberry Pi RP2040 microcontroller, specifying how OpenOCD
+# 'rp2350.cfg' is the configuration file for the Raspberry Pi RP2350 microcontroller, specifying how OpenOCD
 # should interact with this specific target. It sets up the necessary parameters for OpenOCD to correctly
-# control and debug the RP2040.
+# control and debug the RP2350.
 
 # -c "adapter speed 5000"
 # The '-c' option allows you to pass commands to OpenOCD directly from the command line.
 # In this case, "adapter speed 5000" sets the communication speed between the debugger (CMSIS-DAP adapter)
-# and the target (RP2040) to 5000 kHz (5 MHz). This is a high speed, often used to ensure fast
+# and the target (RP2350) to 5000 kHz (5 MHz). This is a high speed, often used to ensure fast
 # communication during debugging. The speed value may need to be adjusted based on the capabilities of
 # the debug adapter or the stability of the connection with the target device.
 
-# The configuration files such as cmsis-dap.cfg and rp2040.cfg are included with the installation of OpenOCD. 
+# The configuration files such as cmsis-dap.cfg and rp2350.cfg are included with the installation of OpenOCD. 
 # These files provide pre-configured settings for various debug interfaces (like CMSIS-DAP) and
-# target devices (such as the RP2040 microcontroller).
+# target devices (such as the RP2350 microcontroller).
