00343ae7d2aca0b2c96d64553892180db63c7f2f
blankMCU <vexown@gmail.com>
2025-02-25 17:49:53 +0100

    Add defines such as OTA server IP and HTTPS port. Increase TCP RECV buffer.

--- UnitApplication/SWC/WiFi/Include/WiFi_Common.h
+++ UnitApplication/SWC/WiFi/Include/WiFi_Common.h
@@ -4,13 +4,15 @@
 /* Server defines */
 #define EXTERNAL_SERVER_IP_ADDRESS  "192.168.4.1"    //Either UDP or TCP external server IP address that we communicate with (e.g PC, another Pico), statically configured on that server
 #define PICO_W_STATIC_IP_ADDRESS    "192.168.1.50"   //Static IP address for the Pico W
+#define OTA_HTTPS_SERVER_IP_ADDRESS "192.168.1.194"  //IP address of the HTTPS server for OTA updates (Apache2 server on the PC)
 #define NETMASK_ADDR                "255.255.255.0"  //Subnet mask for the network (meaning you can use 192.168.1.1 to 192.168.1.254 for devices 
                                                      //(the 0 address is the network address and 255 is the broadcast address).
 #define GATEWAY_ADDR                "192.168.1.1"    //Gateway IP address (in this case the Tenda Wifi router is the gateway so it's its' address)
 #define SERVER_PORT                 (uint16_t)12345  //TODO - server port for testing, later you can think about which one to use permanently
 #define TCP_PORT                    8080
 #define TCP_HTTP_PORT               80
-#define TCP_RECV_BUFFER_SIZE        1024             //in bytes
+#define OTA_HTTPS_SERVER_PORT       443
+#define TCP_RECV_BUFFER_SIZE        65536             //in bytes
 #define UDP_SERVER_PORT             5000
 #define UDP_CLIENT_PORT             5001
 #define UDP_RECV_BUFFER_SIZE        1024
@@ -20,6 +22,7 @@
 #define PICO_TRANSITION_TO_ACTIVE_MODE  1
 #define PICO_TRANSITION_TO_LISTEN_MODE  2
 #define PICO_TRANSITION_TO_MONITOR_MODE 3
+#define PICO_TRANSITION_TO_UPDATE_MODE  4
 
 /* Non-specific functions */
 bool connectToWifi(void);
